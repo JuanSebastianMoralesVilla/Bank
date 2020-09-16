8491e75c1519e3d77c50170e0df28e2c2f184e9f
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-09-16 02:55:50 -0500

    Adelanto clase bank
    codigo
    organizar un fondo en una carpeta

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -2,4 +2,33 @@ package model;
 
 public class Bank {
 
+	private String name;
+
+	public void addClient() {
+
+	}
+
+	public double retirement(double amount) {
+		return 0;
+	}
+
+	public double consignment(double amount, String idAccount) {
+		return 0;
+	}
+
+	public boolean cancelAccount(String idAccount) {
+
+		return false;
+	}
+
+	public boolean payTarjet(String idtarjet) {
+
+		return true;
+	}
+
+	public Client searchUser(String name, String id) {
+
+		return null;
+	}
+
 }
---
+++ src/model/Shifth.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Shifth {
+	
+	private String idShifth;
+
+	
+	
+	public Shifth() {
+		idShifth=null;
+	}
+	public String getIdShifth() {
+		return idShifth;
+	}
+
+	public void setIdShifth(String idShifth) {
+		this.idShifth = idShifth;
+	}
+	
+	
+
+}
