e3bd6278013086fa639c4be7b78dcf8e041b361f
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-09-17 17:26:22 -0500

    Ventana inicial
    interfaces menu de opciones

--- src/ui/BankGUI.java
+++ src/ui/BankGUI.java
@@ -1,5 +1,25 @@
 package ui;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import model.Bank;
+
 public class BankGUI {
+	
+	private Bank bank;
+	
+
+	   
+	   
+	BankGUI(Stage ventana, Bank bank){
+		this.bank= bank;
+		
+	}
+
+	
+	
 
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,47 @@
 package ui;
 
-public class Main {
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import model.Bank;
+import model.*;
+import ui.*;
+
+public class Main extends Application {
+
+	private BankGUI bankGui;
+	private Bank bank;
+
+	public Main() {
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+
+		FXMLLoader fxmlLoad = new FXMLLoader(getClass().getResource("WelcomeInicial.fxml"));
+		bankGui = new BankGUI(stage, bank);
+		fxmlLoad.setController(bankGui);
+		Parent root = fxmlLoad.load();
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("iconos.css").toExternalForm());
+		stage.setScene(scene);
+		stage.centerOnScreen();
+		stage.setMinHeight(600);
+		stage.setMinWidth(1000);
+        stage.setTitle("Bank");
+		stage.show();
 	}
 
 }
