718610e311ffc34c6dff034ea447e22f83240e76
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-09-22 01:34:41 -0500

    correcciones en el Ui

--- src/ui/BankGUI.java
+++ src/ui/BankGUI.java
@@ -1,56 +1,72 @@
 package ui;
 
-import java.awt.Button;
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import model.Bank;
+import threads.ValuesEmptyThread;
 
 public class BankGUI {
-	
+
 	private Bank bank;
 
-	//Elements of interface
-@FXML
-private Pane mainPane;
-@FXML
-private Button btInit;
-   
+	// Elements of interface
+	@FXML
+	private Pane mainPane;
+	@FXML
+	private Button btInit;
 
+	// turn
+
+	@FXML
+	private Button btShift;
+
+	@FXML
+	private TextField txtName1;
+
+	@FXML
+	private TextField txtid1;
+
+	@FXML
+	private Button btcreateUser;
 
 //Constructor
-	   
-	BankGUI(Stage ventana, Bank bank){
-		this.bank= bank;
-		
+
+	public BankGUI(Stage stage, Bank bank) {
+		// TODO Auto-generated constructor stub
+		this.bank = bank;
+
 	}
 
+	
+	public void init(ActionEvent event) throws Exception {
 
-    @FXML
-    void init(ActionEvent event) throws Exception {
-    	System.out.println("entre");
-    	  try {
-              FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SearchID.fxml"));
-                      Parent root1 = (Parent) fxmlLoader.load();
-                      Stage stage = new Stage();
-                      stage.setScene(new Scene(root1));  
-                      stage.show();
-                      System.out.println("Entre");
-              } catch(Exception e) {
-                 e.printStackTrace();
-                }
-    	  
-    }
-    
-    
-    }
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MenuOptions.fxml"));
+			fxmlLoader.setController(this);
+			Parent root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root1));
+			stage.show();
+			System.out.println("Entre");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    	
-    	
+	}
+	
+	
 
+
+
+
+	
+}
