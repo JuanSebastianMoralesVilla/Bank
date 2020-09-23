2f79cb16ed85c095ab4ae5e5696d068b8ad7728a
JuanSebastianMoralesVilla <juansebastianmoralesvilla@gmail.com>
2020-09-23 01:09:15 -0500

    Menu Navegable

--- src/ui/BankGUI.java
+++ src/ui/BankGUI.java
@@ -14,6 +14,8 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import model.Bank;
@@ -23,58 +25,157 @@ public class BankGUI {
 	private Bank bank;
 
 	// Elements of interface
-	
 
-    @FXML
-    private BorderPane myPane;
-	
+	@FXML
+	private BorderPane myPane;
+
 	@FXML
 	private Button btInit;
 
+	/// fxml de la creacion de clientes
 
-    
-    
+	private Button btnextUser;
 
-	 /// fxml de la creacion de clientes
-    
-    private Button btnextUser;
+	@FXML
+	private TextField txtName;
 
-    @FXML
-    private TextField txtName;
+	@FXML
+	private TextField txtLastName;
 
-    @FXML
-    private TextField txtLastName;
+	@FXML
+	private TextField txtIDaddUser;
 
-    @FXML
-    private TextField txtIDaddUser;
+	@FXML
+	private BorderPane BorderPaint1;
 
-    
-    
-    
-   
-    
-    @FXML
-    private BorderPane BorderPaint1;
+	@FXML
+	private Pane mainPaneCreateUser;
 
-    @FXML
-    private Pane mainPaneCreateUser;
+	// interfaz 2 de creacion
 
- 
+	@FXML
+	private Button btCreateUser;
 
-    @FXML
-    private ToggleGroup grupoTipocliente;
+	@FXML
+	private DatePicker txtDate;
 
-   
+	@FXML
+	private TextField txtIdTarjet;
+
+	@FXML
+	private TextField txtamount;
+
+	@FXML
+	private ToggleGroup GroupTarjet;
+
+	@FXML
+	private ToggleGroup grupoTipocliente;
+
+	// interfaz de buscar id
+
+	@FXML
+	private Button btSearch;
+
+	@FXML
+	private TextField txtid;
+
+	@FXML
+	private Button btAtrasSearch;
+
+	// interfaz de turno
+
+	@FXML
+	private Button btShift;
+
+	@FXML
+	private TextField txtName1;
+
+	@FXML
+	private TextField txtid1;
+
+	@FXML
+	private Button btcreateUser;
 
 //Constructor
-
 
 	public BankGUI(Stage stage, Bank bank) {
 		// TODO Auto-generated constructor stub
 	}
 
 	public void init(ActionEvent event) throws IOException {
-	
+
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("turnoInterface.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+
+		/*
+		 * FXMLLoader f= new FXMLLoader(getClass().getResource("CreateUser1.fxml"));
+		 * f.setController(this); Parent root= f.load(); Stage st=new Stage();
+		 * 
+		 * sc.getStylesheets().add(getClass().getResource("iconos.css").toExternalForm()
+		 * ); st.initModality(Modality.APPLICATION_MODAL);
+		 * st.initStyle(StageStyle.DECORATED); st.setScene(sc);
+		 * 
+		 * st.show();
+		 * 
+		 */
+	}
+
+	@FXML
+	void nextUser(ActionEvent event) throws IOException {
+
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("createUser2.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+
+	}
+
+	// Boton de Crear el usuario
+	@FXML
+	void createUser(ActionEvent event) throws IOException {
+
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("turnoInterface.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+	}
+
+	@FXML
+	void btAtras(ActionEvent event) throws IOException {
+
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("MenuOptions.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+		System.out.println("entre");
+
+	}
+
+	@FXML
+	void btSearchIDtoOptions(ActionEvent event) throws IOException {
+
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("TableInterface.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+
+	}
+
+	@FXML
+	void addUser(ActionEvent event) throws IOException {
+
 		FXMLLoader fL = new FXMLLoader(getClass().getResource("CreateUser1.fxml"));
 		fL.setController(this);
 		Parent pane;
@@ -82,109 +183,96 @@ public class BankGUI {
 		myPane.getChildren().clear();
 		myPane.setCenter(pane);
 
-		
-		
-		
-		/*
-	FXMLLoader f= new FXMLLoader(getClass().getResource("CreateUser1.fxml"));
-	f.setController(this);
-	Parent root= f.load();
-	Stage st=new Stage();
-	
-	sc.getStylesheets().add(getClass().getResource("iconos.css").toExternalForm());
-	st.initModality(Modality.APPLICATION_MODAL);
-st.initStyle(StageStyle.DECORATED);
-st.setScene(sc);
+	}
 
-st.show();
+	@FXML
+	void createShift(ActionEvent event) throws IOException {
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("reportQueue.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+	}
 
-*/
+	@FXML
+	void loadInformationClient(ActionEvent event) throws IOException {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TableInterface.fxml"));
+		fxmlLoader.setController(this);
+		Parent root1 = (Parent) fxmlLoader.load();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root1));
+		stage.show();
+	}
+
+	// Tabla de ordenamientos
+
+	@FXML
+	private TableView<?> Table1;
+
+	@FXML
+	private TableColumn<?, ?> TcNombre;
+
+	@FXML
+	private TableColumn<?, ?> TcCedula;
+
+	@FXML
+	private TableColumn<?, ?> TcVinculacion;
+
+	@FXML
+	private TableColumn<?, ?> TcMonto;
+
+	@FXML
+	private Button BtSelecition;
+
+	@FXML
+	private Button btHeapsort;
+
+	@FXML
+	private Button btMergesort;
+
+	@FXML
+	private Button btquicksort;
+
+	@FXML
+	private Button btatrasTable;
+
+	@FXML
+	void atrasTableInterface(ActionEvent event) throws IOException {
+
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("turnoInterface.fxml"));
+		fL.setController(this);
+		Parent pane;
+		pane = fL.load();
+		myPane.getChildren().clear();
+		myPane.setCenter(pane);
+
 	}
 	
 	
+	// interfaz prioriatria de turno
 	
     @FXML
-    void nextUser(ActionEvent event) throws IOException {
-    	
-    	FXMLLoader fL = new FXMLLoader(getClass().getResource("createUser2.fxml"));
+    private Button btOptionUser;
+    
+    @FXML
+    private TextField txtStackNormal;
+
+    @FXML
+    private TextField txtStackPriority;
+
+    @FXML
+    private Button btnextTurn;
+
+    @FXML
+    void loadMenuOptions(ActionEvent event) throws IOException {
+		FXMLLoader fL = new FXMLLoader(getClass().getResource("MenuOptions.fxml"));
 		fL.setController(this);
 		Parent pane;
 		pane = fL.load();
 		myPane.getChildren().clear();
 		myPane.setCenter(pane);
-
+		System.out.println("entre");
     }
-    
-    // interfaz 2 de creacion 
-    
-    @FXML
-    private Button btCreateUser;
-
-    @FXML
-    private DatePicker txtDate;
-
-    @FXML
-    private TextField txtIdTarjet;
-
-    @FXML
-    private TextField txtamount;
-
-    @FXML
-    private ToggleGroup GroupTarjet;
-
-    @FXML
-    void createUser(ActionEvent event) throws IOException {
-
-    	
-       	FXMLLoader fL = new FXMLLoader(getClass().getResource("SearchID.fxml"));
-    		fL.setController(this);
-    		Parent pane;
-    		pane = fL.load();
-    		myPane.getChildren().clear();
-    		myPane.setCenter(pane);
-    }
-    
-    
-    
-    
-    // interfaz de buscar id
-    
-    @FXML
-    private Button btSearch;
-
-    @FXML
-    private TextField txtid;
-
-    @FXML
-    private Button btAtrasSearch;
-
-    @FXML
-    void btAtras(ActionEvent event) throws IOException {
-    
-       	FXMLLoader fL = new FXMLLoader(getClass().getResource("MenuOptions.fxml"));
-    		fL.setController(this);
-    		Parent pane;
-    		pane = fL.load();
-    		myPane.getChildren().clear();
-    		myPane.setCenter(pane);
-    		System.out.println("entre");
-        	
-
-    }
-
-    @FXML
-    void btSearchIDtoOptions(ActionEvent event) throws IOException {
-    	
-    	FXMLLoader fL = new FXMLLoader(getClass().getResource("TableInterface.fxml"));
-		fL.setController(this);
-		Parent pane;
-		pane = fL.load();
-		myPane.getChildren().clear();
-		myPane.setCenter(pane);
-		
-  
-    }
-
-    
-
 }
